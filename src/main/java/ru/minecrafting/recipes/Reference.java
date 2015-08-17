1446f64355ef6caee0eaf772ae4c5b97e10c137c
Meegoo <Meegoo100@gmail.com>
2015-08-17 14:18:42 +0500

    Initial Commit

---
+++ src/main/java/ru/minecrafting/recipes/Reference.java
@@ -0,0 +1,8 @@
+package ru.minecrafting.recipes;
+
+public class Reference {
+    public static final String MODID = "MinecraftingRecipes";
+    public static final String VERSION = "1.2";
+    public static final String MOD_NAME = "Minecrafting Recipes";
+
+}
