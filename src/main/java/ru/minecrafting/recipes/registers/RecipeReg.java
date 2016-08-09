8a9aa53d13c252545eb69a4ae9c89cb0d7b231e3
Meegoo <Meegoo100@gmail.com>
2016-08-09 13:51:37 +0500

    Moved buildcraft recipes to appropriate location

--- src/main/java/ru/minecrafting/recipes/registers/RecipeReg.java
+++ src/main/java/ru/minecrafting/recipes/registers/RecipeReg.java
@@ -43,6 +43,7 @@ public class RecipeReg {
 	}
 
 	private static void registerVanillaRecipes() {
+		registerBuildcraftRecipes();
 		GameRegistry.addShapedRecipe(new ItemStack(ItemReg.eldritchResearchNotes, 1, 42), "rrr", "rrr", "rrr", 'r', new ItemStack(ItemReg.MRThaumcraft, 1, 1));
 		cleric_helmet = GameRegistry.addShapedRecipe(new ItemStack(ConfigItems.itemHelmetCultistRobe), "r r", "rrr", "rrr", 'r', new ItemStack(ItemReg.MRThaumcraft, 1, 3));
 		cleric_chest = GameRegistry.addShapedRecipe(new ItemStack(ConfigItems.itemChestCultistRobe), "rrr", "r r", "r r", 'r', new ItemStack(ItemReg.MRThaumcraft, 1, 3));
@@ -123,5 +124,58 @@ public class RecipeReg {
 		blood_ingot = ThaumcraftApi.addShapelessArcaneCraftingRecipe("CRIMSON_KNIGHT_ARMOR", new ItemStack(ItemReg.MRThaumcraft, 1, 4),
 				new AspectList().add(Aspect.ORDER, 4).add(Aspect.ENTROPY, 4).add(Aspect.EARTH, 8).add(Aspect.WATER, 4),
 				new ItemStack(ConfigItems.itemResource, 1, 2), new ItemStack(ItemReg.MRThaumcraft, 1, 0));
+	}
+
+	private static void registerBuildcraftRecipes() {
+		OreDictionary.registerOre("gravel", new ItemStack(Blocks.gravel));
+		String[] ingredients = new String[10];
+		Item[] results = new Item[10];
+
+		ingredients[0] = "plankWood";
+		ingredients[1] = "cobblestone";
+		ingredients[2] = "stone";
+		ingredients[3] = "gemDiamond";
+		ingredients[4] = "ingotGold";
+		ingredients[5] = "ingotIron";
+		ingredients[6] = "gemQuartz";
+		ingredients[7] = "cobblestone";
+		ingredients[8] = "gemEmerald";
+		ingredients[9] = "sandstone";
+
+		results[0] = BuildCraftTransport.pipePowerWood;
+		results[1] = BuildCraftTransport.pipePowerCobblestone;
+		results[2] = BuildCraftTransport.pipePowerStone;
+		results[3] = BuildCraftTransport.pipePowerDiamond;
+		results[4] = BuildCraftTransport.pipePowerGold;
+		results[5] = BuildCraftTransport.pipePowerIron;
+		results[6] = BuildCraftTransport.pipePowerQuartz;
+		results[7] = BuildCraftTransport.pipeStructureCobblestone;
+		results[8] = BuildCraftTransport.pipePowerEmerald;
+		results[9] = BuildCraftTransport.pipePowerSandstone;
+
+
+		try {
+			if (ingredients.length != results.length)
+				throw new ArrayIndexOutOfBoundsException();
+			for (int n = 0; n < results.length; n++) {
+				String modifier;
+				String name;
+
+				name = results[n].getUnlocalizedName();
+				if (name.contains("Power")) {
+					modifier = "dustRedstone";
+				} else if (name.contains("Structure")) {
+					modifier = "gravel";
+				} else {
+					modifier = null;
+				}
+
+
+				GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(results[n], 8), "rrr", "igi", "rrr", 'r', modifier, 'i', ingredients[n], 'g', "blockGlass"));
+			}
+		} catch (ArrayIndexOutOfBoundsException exception) {
+			LogHelper.error("Cannot register recipes");
+			exception.printStackTrace();
+		}
 	}
 }
