ddb6b47c52d548c09a8bbf36903301d5a51a2788
Meegoo <Meegoo100@gmail.com>
2016-08-09 13:50:02 +0500

    Added aspects to all items

--- src/main/java/ru/minecrafting/recipes/registers/AspectReg.java
+++ src/main/java/ru/minecrafting/recipes/registers/AspectReg.java
@@ -7,7 +7,11 @@ import thaumcraft.api.aspects.AspectList;
 
 public class AspectReg {
 	public static void registerAspects() {
+		ThaumcraftApi.registerObjectTag(new ItemStack(ItemReg.MRThaumcraft, 1, 0),
+				new AspectList().add(Aspect.MAGIC, 6).add(Aspect.BEAST, 2).add(Aspect.SLIME, 2));
+		ThaumcraftApi.registerObjectTag(new ItemStack(ItemReg.MRThaumcraft, 1, 1),
+				new AspectList().add(Aspect.MIND, 8).add(Aspect.ELDRITCH, 2));
 		ThaumcraftApi.registerObjectTag(new ItemStack(ItemReg.MRThaumcraft, 1, 2),
-				new AspectList().add(Aspect.EARTH,1).add(Aspect.WATER,1).add(Aspect.FIRE,1).add(Aspect.ORDER,1).add(Aspect.AIR,1).add(Aspect.ENTROPY,1));
+				new AspectList().add(Aspect.EARTH, 1).add(Aspect.WATER, 1).add(Aspect.FIRE, 1).add(Aspect.ORDER, 1).add(Aspect.AIR, 1).add(Aspect.ENTROPY, 1));
 	}
 }
